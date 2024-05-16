d832dd117584a3a0589030a198c22b25eac5cbc3
sora11022 <139995480+sora11022@users.noreply.github.com>
2024-05-16 21:30:44 +0700

    Delete btjs5 directory

--- btjs5/bai5.js
+++
@@ -1,5 +0,0 @@
-const arr1 = [1, 2, 3, 4, 5];
-const arr2 = [2, 3, 6];
-
-let difference = arr1.filter(x => !arr2.includes(x)).concat(arr2.filter(x => !arr1.includes(x)));
-console.log(difference);
--- btjs5/bai7.js
+++
@@ -1,7 +0,0 @@
-const arrCount = [1, 2, 3, 4, 5, 2, 3, 4, 2, 2, 7, 8, 8];
-
-const countNum = arrCount.reduce((acc, curr) => {
-    acc[curr] = (acc[curr] || 0) + 1;
-    return acc;
-}, {});
-console.log(countNum);
