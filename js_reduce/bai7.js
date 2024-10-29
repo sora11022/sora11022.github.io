9f5d10d63813113d495c410e40cf179c5b1b9c1d
sora11022 <tuongdc.odin@gmail.com>
2024-10-29 09:55:01 +0700

    Delete js_reduce directory

--- js_reduce/bai7.js
+++
@@ -1,6 +0,0 @@
-const arrCount = [1, 2, 3, 4, 5, 2, 3, 4, 2, 2, 7, 8, 8];
-const result = arrCount.reduce((pre, cur) =>{
-    pre[cur] = (pre[cur] || 0) + 1;
-    return pre;
-}, {})
-console.log(result);
